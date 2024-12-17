adf30b5c31384bfdfb74c3d4b7d15f69970315d1
phuongtung0801 <63737354+phuongtung0801@users.noreply.github.com>
2024-12-17 10:05:58 +0700

    Update esp32_reset.sh

--- esp32_reset.sh
+++ esp32_reset.sh
@@ -2,7 +2,7 @@
 
 # Kiểm tra xem script có được chạy với sudo không
 if [ "$EUID" -ne 0 ]; then 
-    echo -e "\e[31mError: Script này phải được chạy với quyền sudo\e[0m"
+    echo -e "\e[31mError: Script này phải được chạy với quyền sudo ahihi\e[0m"
     echo "Vui lòng chạy: sudo $0"
     exit 1
 fi
